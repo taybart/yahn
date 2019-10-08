c80a82941e569c3f5270c4ed1ee7cfc24aa97da9
Taylor <taybart@gmail.com>
2019-10-07 20:48:06 -0600

    Initial Commit

---
+++ yahn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yahn
+//
+//  Created by Taylor on 10/7/19.
+//  Copyright © 2019 Taylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
